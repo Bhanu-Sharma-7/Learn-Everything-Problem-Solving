acee13b97f71955e2b625ff4d4deef8014a8e2ad
Bhanu-Sharma-7 <bhanusharma14581@gmail.com>
2026-01-03 23:14:41 +0530

    day 3 on leetcode

---
+++ romanToInteger.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+class Solution {
+public:
+    int romanToInt(std::string s) {
+        // 1. Map symbols to their integer values
+        std::unordered_map<char, int> romanMap = {
+            {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}
+        };
+        
+        int total = 0;
+        int n = s.length();
+        
+        for (int i = 0; i < n; i++) {
+            // 2. If current value is smaller than the next, subtract it
+            // Otherwise, add it.
+            if (i < n - 1 && romanMap[s[i]] < romanMap[s[i + 1]]) {
+                total -= romanMap[s[i]];
+            } else {
+                total += romanMap[s[i]];
+            }
+        }
+        
+        return total;
+    }
+};
